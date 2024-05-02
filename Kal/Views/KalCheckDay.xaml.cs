9f52081074affac79cee13e6de3c7afe61930faf
egorkaw2w <85741587+egorkaw2w@users.noreply.github.com>
2024-05-02 22:09:05 +0300

    ну тут есть фронт

--- Kal/Views/KalCheckDay.xaml.cs
+++ Kal/Views/KalCheckDay.xaml.cs
@@ -1,27 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace Kal.Views
 {
-    /// <summary>
-    /// Логика взаимодействия для KalCheckDay.xaml
-    /// </summary>
     public partial class KalCheckDay : Window
     {
         public KalCheckDay()
         {
             InitializeComponent();
+
+            SolidColorBrush animatedBrush = new SolidColorBrush(Colors.LightBlue);
+
+            VeryCool.Background = animatedBrush;
+
+            ColorAnimation colorAnimation = new ColorAnimation
+            {
+                To = Colors.Green,
+                Duration = new Duration(TimeSpan.FromSeconds(1)),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+            animatedBrush.BeginAnimation(SolidColorBrush.ColorProperty, colorAnimation);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow wind = new MainWindow();
+            wind.Show();
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow wind = new MainWindow();
+            wind.Show();
+            Close();
         }
     }
 }
--- Kal/Views/MainWindow.xaml.cs
+++ Kal/Views/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System.Text;
+﻿using Kal.Views;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -19,6 +21,27 @@ namespace Kal
         public MainWindow()
         {
             InitializeComponent();
+            
+            DoubleAnimation cool = new DoubleAnimation();
+            cool.From = CoolKnopka.ActualHeight;
+            cool.To = 150;
+            cool.Duration = TimeSpan.FromSeconds(3.14);
+            CoolKnopka.BeginAnimation(Button.HeightProperty, cool); 
+
+            DoubleAnimation cool2 = new DoubleAnimation();
+            cool2.From = NotCoolButton.ActualWidth;
+            cool2.To = 369;
+            cool2.Duration = TimeSpan.FromSeconds(3.15);
+            NotCoolButton.BeginAnimation(Button.WidthProperty, cool2);
         }
+
+        private void KalDay_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            KalCheckDay windo = new KalCheckDay();
+            windo.Show();
+            Close();
+        }
+
+
     }
 }
